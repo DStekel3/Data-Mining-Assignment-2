864125f691ebec2ab47e8394fb46cb78429bcf02
D.J. Stekelenburg <d.j.stekelenburg@students.uu.nl>
2017-11-02 12:02:07 +0100

    Added GetTestset() again :)

--- DataLoader.R
+++ DataLoader.R
@@ -22,17 +22,34 @@ GetTrainset <- function(){
   # Make all letters lower case
   reviews.trainset.all <- tm_map(reviews.trainset.all,content_transformer(tolower))
   # Remove stopwords
-  reviews.trainset.all <- tm_map(reviews.trainset.all, removeWords,
-                                 stopwords("english"))
+  reviews.trainset.all <- tm_map(reviews.trainset.all, removeWords, stopwords("english"))
   # Remove numbers
   reviews.trainset.all <- tm_map(reviews.trainset.all,removeNumbers)
   # Remove excess whitespace
   reviews.trainset.all <- tm_map(reviews.trainset.all,stripWhitespace)
-  
-  reviews.trainset.all <- tm_map(reviews.trainset.all, stemDocument)
-  
-  reviews.trainset.all <- tm_map(reviews.trainset.all, PlainTextDocument)
   return(reviews.trainset.all)
+}
+
+# Get testset as a Corpus
+GetTestset <- function(){
+  # load in the testset
+  reviews.testset.dec <- VCorpus(DirSource("dataset/testset/dec", recursive = TRUE, encoding="UTF-8"))
+  
+  reviews.testset.tru <- VCorpus(DirSource("dataset/testset/tru", recursive = TRUE, encoding="UTF-8"))
+  reviews.testset.all <- c(reviews.testset.dec, reviews.testset.tru)
+  
+  
+  # Remove punctuation marks (commas, etc.)
+  # reviews.testset.all <- tm_map(reviews.testset.all,removePunctuation)
+  # 
+  # # Make all letters lower case
+  # reviews.testset.all <- tm_map(reviews.testset.all,content_transformer(tolower))
+  # # Remove stopwords
+  # reviews.testset.all <- tm_map(reviews.testset.all, removeWords, stopwords("english"))
+  # # Remove numbers
+  # reviews.testset.all <- tm_map(reviews.testset.all,removeNumbers)
+  # # Remove excess whitespace
+  # reviews.testset.all <- tm_map(reviews.testset.all,stripWhitespace)
 }
 
 BigramTokenizer <- function(x) {NGramTokenizer(x, Weka_control(min = 2, max = 2))}
--- NaiveBayes.R
+++ NaiveBayes.R
@@ -46,22 +46,25 @@ trainset.labels <- c(rep(0,320), rep(1,320))
 # create document-term matrix from training corpus
 train.dtm <- DocumentTermMatrix(trainset)
 
-# remove feature that occus in less than 2% of the documents
-train.dtm <- removeSparseTerms(train.dtm,0.98)
+# remove feature that occus in less than 5% of the documents
+train.dtm <- removeSparseTerms(train.dtm,0.95)
 
 # uncomment the next line for bigrams
-train.dtm <- GetTrainsetBi()
+# train.dtm <- GetTrainsetBi()
 
 # train the naive bayes multinomial classifier using the trainset
 reviews.mnb <- train.mnb(as.matrix(train.dtm),trainset.labels)
 
 testset <- GetTestset()
 
+
 testset.labels <- c(rep(0, 80), rep(1,80))
 
 # create document term matrix for test set
 test.dtm <- DocumentTermMatrix(testset, list(dictionary=dimnames(train.dtm)[[2]]))
-test.dtm <- GetTestsetBi()
+
+# uncomment the next line for bigrams
+# test.dtm <- GetTestsetBi()
 
 reviews.mnb.pred <- predict.mnb(reviews.mnb,as.matrix(test.dtm))
 
